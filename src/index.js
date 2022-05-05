ea88d1d7cd41c4cb0d953045658b82382b01b59d
Naveen kumar kumawat <kumawatnaveen17@gmail.com>
2022-05-05 13:06:18 +0530

    #

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// require('dotenv').config(); 
+// require('dotenv').config();
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const app = express();
 const hbs = require("hbs");
 // const bcrypt = require("bcryptjs");
@@ -11,7 +11,7 @@ const hbs = require("hbs");
 // const Rohit = require("./models/registration");
 // const { json } = require("express");
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
@@ -24,47 +24,54 @@ const partials_path = path.join(__dirname, "../templates/partials");
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
 app.set("views", template_path);
-hbs.registerPartials(partials_path); 
+hbs.registerPartials(partials_path);
 
-app.get("/", (req , res) => {
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/dmag", (req , res)=>{
-    res.render("Dmag");
+app.get("/dmag", (req, res) => {
+  res.render("Dmag");
 });
 
-app.get("/team", (req , res)=>{
-    res.render("Final team")
+app.get("/team", (req, res) => {
+  res.render("Final team");
 });
 
-app.get("/ai", (req, res)=>{
-    res.render("aiWebsite")
+app.get("/ai", (req, res) => {
+  res.render("aiWebsite");
 });
 
-app.get("/blockchain", (req, res)=>{
-    res.render("blockchain")
+app.get("/blockchain", (req, res) => {
+  res.render("blockchain");
 });
 
-app.get("/cloud", (req, res)=>{
-    res.render("cloud")
+app.get("/cloud", (req, res) => {
+  res.render("cloud");
 });
 
-app.get("/cyber", (req, res)=>{
-    res.render("cyber")
+app.get("/cyber", (req, res) => {
+  res.render("cyber");
 });
 
-app.get("/research", (req, res)=>{
-    res.render("research")
+app.get("/research", (req, res) => {
+  res.render("research");
 });
 
-app.get("/software", (req, res)=>{
-    res.render("software")
+app.get("/software", (req, res) => {
+  res.render("software");
+});
+
+app.get("/sign_in", (req, res) => {
+  res.render("sign_in");
+});
+app.get("/sign_up", (req, res) => {
+  res.render("sign_up");
 });
 
 // app.get("/list", auth , (req , res)=>{
 //     console.log(`cookie value ${req.cookies.user}`)
-//     res.render("list"); 
+//     res.render("list");
 // });
 
 // app.get("/logout", auth , async(req , res) =>{
@@ -97,7 +104,7 @@ app.get("/software", (req, res)=>{
 //             return res.status(422).render("signup",{error: "password are not matched"});
 //         }else{
 //                const registeruserdata = new Rohit({ myname, myemail, mypassword, mycpassword});
-                  
+
 //                 const token = await registeruserdata.produceAuthToken();
 
 //             res.cookie("user", token, {
@@ -106,7 +113,7 @@ app.get("/software", (req, res)=>{
 //             });
 
 //             const registered = await registeruserdata.save();
-//             res.status(201).render( "index" , {display : registered} ); 
+//             res.status(201).render( "index" , {display : registered} );
 //           }
 //          }
 //     catch(error){
@@ -139,11 +146,10 @@ app.get("/software", (req, res)=>{
 //             res.send("data is not correct");
 //         }
 
-
 //     } catch (error) {
 //         res.status(400).send("invalid E-mail")
 //     }
 // })
 app.listen(port, () => {
-    console.log(`server is running at ${port}`)
-})
+  console.log(`server is running at ${port}`);
+});
